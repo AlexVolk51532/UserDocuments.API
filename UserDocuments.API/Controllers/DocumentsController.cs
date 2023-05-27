c6a80e32baf0baa182fafc57883d7c894ae05fa7
Александр Волков <alex_sashok@mail.ru>
2023-05-27 20:04:10 +0600

    Добавьте файлы проекта.

---
+++ UserDocuments.API/Controllers/DocumentsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserDocuments.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentsController : ControllerBase
+    {
+    }
+}
